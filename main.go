3d6f70c230d853b363acbe8b85279a4c0b40d67d
robert <robcampbell@ucmo.edu>
2022-06-28 10:59:14 -0500

    Use regex slice to check file extensions

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import(
 	"fmt"
-	//"io/ioutil"
 	"bufio"
 	"strings"
 	"os"
@@ -11,8 +10,6 @@ import(
 )
 
 func main() {
-
-	// root := "C:/Users/robcampbell/crow_engine"
 
 	var exclude []string
 
@@ -48,20 +45,10 @@ func main() {
 	}
 
 	diggin(root, word, exclude)
-	// testFirst(root, word, exclude)
-}
-
-func testFirst(root string, word string, exclude []string) {
-	fmt.Println("Root is: ", root)
-	fmt.Println("Word is: ", word)
-	for _, v := range exclude {
-		fmt.Println("Exclude: ", v)
-	}
 }
 
 func diggin(root string, word string, excludes []string) {
 
-	//var skip bool = false
 	rgx := []string{"^[A-Za-z0-9]*[.]java$", "^[A-Za-z0-9]*[.]cs$", "^[A-Za-z0-9]*[.]php$", "^[A-Za-z0-9]*[.]html$", "^[A-Za-z0-9]*[.]go$"}
 
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
@@ -69,10 +56,6 @@ func diggin(root string, word string, excludes []string) {
 			fmt.Println(err)
 			return nil
 		}
-
-		// if info.IsDir() && info.Name() == ".git" {
-		// 	return filepath.SkipDir
-		// }
 		
 		if info.IsDir() {
 			for _, dirs := range excludes {
@@ -85,36 +68,32 @@ func diggin(root string, word string, excludes []string) {
 		if !info.IsDir() {
 			for i := 0; i < len(rgx); i++ {
 				reg, _ := regexp.Compile(rgx[i])
-
 				match := reg.MatchString(info.Name())
 
 				if match == true {
-					break
+					p, err := os.Open(path)
+					if err != nil {
+						fmt.Println("messed up opening file")
+						return nil
+					}
+					scanner := bufio.NewScanner(p)
+		
+					for scanner.Scan() {
+						if strings.Contains(scanner.Text(), word) {
+							fmt.Println(path, scanner.Text())
+						} else {
+							continue
+						}
+					}
+				} else {
+					continue
+				}
 			}
-
-			// if skip == true {
-			// 	fmt.Println("should skip: ", info.Name())
-			// 	return filepath.SkipDir
-			// } else {
-			// 	p, err := os.Open(path)
-			// 	if err != nil {
-			// 		fmt.Println("messed up opening file")
-			// 		return nil
-			// 	}
-			// 	scanner := bufio.NewScanner(p)
-	
-			// 	for scanner.Scan() {
-			// 		if strings.Contains(scanner.Text(), word) {
-			// 			fmt.Println(path, scanner.Text())
-			// 		} else {
-			// 			continue
-			// 		}
-			// 	}
-			// }
 		}
 
 		return nil
 	})
+
 	if err != nil {
 		fmt.Println(err)
 	}
